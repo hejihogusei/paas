a322cf2340cf1055c3f36718294378fdf3c0c7d9
hogu <hogu@hejihogusei.net>
2025-05-26 21:15:14 +0200

    initial commit

---
+++ poc/src/lib/Counter.svelte
@@ -0,0 +1,7 @@
+<script>
+	let count = 0;
+</script>
+
+<button on:click={() => count++} class="bg-blue-500 text-white px-4 py-2 rounded">
+	Count: {count}
+</button>
---
+++ poc/src/routes/hello/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Counter from '$lib/Counter.svelte';
+	let name = 'world';
+</script>
+
+<Counter />
+
+<h1 class="text-3xl font-bold text-blue-600">Hello {name}!</h1>
+<input bind:value={name} class="mt-4 p-2 border" placeholder="Type your name..." />
